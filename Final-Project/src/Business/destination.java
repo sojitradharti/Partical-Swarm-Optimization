5f75e1398c0498b1babf8b23c31a11647e9d2bb2
KaminiPremPrakash <46829105+KaminiPremPrakash@users.noreply.github.com>
2019-08-11 12:07:35 -0400

    Adding 2 basic java files for PSO

---
+++ Final-Project/src/Business/destination.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author sojit
+ */
+public class destination {
+
+   
+Location[] Locations;
+    destination(int destinationSize) {
+        Locations = new Location[destinationSize];
+        for(int i = 0 ;i<destinationSize;i++)
+        {
+            Location l = new Location(i);
+            Locations[i] = l;
+        }
+       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+  
+    
+}
